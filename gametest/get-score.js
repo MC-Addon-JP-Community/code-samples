9f64ec5d38f2ad9b2170834ddd6289b21452d451
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2022-10-30 15:03:22 +0900

    Create get-score.js

---
+++ gametest/get-score.js
@@ -0,0 +1,16 @@
+/**
+ * フェイクプレイヤー対応版のスコア取得関数
+ * @author akki256
+ * @param {import('@minecraft/server').Player|import('@minecraft/server').Entity|string} target
+ * @param {string} objective
+ * @returns {number|null}
+ */
+export function getScore(target, objective) {
+    try {
+        if (typeof target === 'string') return world.scoreboard.getObjective(objective).getScores().find(({ participant }) => participant.displayName === target).score;
+        return world.scoreboard.getObjective(objective).getScore(target.scoreboard);
+    }
+    catch {
+        return null;
+    }
+}
